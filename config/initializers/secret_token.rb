3bf34e979e0a0e6e7c87ca5c0e9721459bee89a0
albert_kai <albertkai@me.com>
2013-04-06 18:49:46 +0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skin::Application.config.secret_token = 'c8768a616ced1ab3345baaa93c6b16497a328f0dd6fe133a33925c5dcef6412371dd3bd9196a65e39eb14d7bba9aa1377ed30f4d29e2c7f730cd3011cde21299'
